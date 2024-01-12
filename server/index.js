5931dbfbd010379e2cdf315fe8fadce96fdf406d
kaushikgarg0209 <kaushikgarg0209@gmail.com>
2024-01-12 21:49:04 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,17 @@ app.use(cors({
     credentials: true,
     optionsSuccessStatus: 204,
   }));
-  
+
+app.use(function(req, res, next) {
+    res.header('Content-Type', 'application/json;charset=UTF-8')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
+
 app.use(express.json());
 
 app.use("/admin", adminRouter)
