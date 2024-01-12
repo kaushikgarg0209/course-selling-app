3c235a0346b244576630026c66147523747c1594
kaushikgarg0209 <kaushikgarg0209@gmail.com>
2024-01-12 22:30:40 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -7,19 +7,18 @@ const userRouter = require("./routes/user");
 const app = express();
 
 app.use(cors({
-    credentials : true,
-    origin: 'http://localhost:5173'
+    credentials: true,
+    origin: 'https://course-selling-86cu18ca4-kaushikgarg0209.vercel.app'
 }));
 
 app.use(function(req, res, next) {
-    res.header('Content-Type', 'application/json;charset=UTF-8')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-})
+    res.header('Content-Type', 'application/json;charset=UTF-8');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', 'https://course-selling-86cu18ca4-kaushikgarg0209.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 
 app.use(express.json());
 
