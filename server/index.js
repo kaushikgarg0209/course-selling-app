671fcc0cf4670739d736f449e7a95e9ff5212fc0
kaushikgarg0209 <kaushikgarg0209@gmail.com>
2024-01-12 21:05:36 +0530

    handle cors

--- server/index.js
+++ server/index.js
@@ -7,20 +7,12 @@ const userRouter = require("./routes/user");
 const app = express();
 
 app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    origin: 'https://course-selling-app-weld.vercel.app/'
-}));
-
-app.use(function(req, res, next){
-    res.header('Content-Type', 'appication/json;charset==UTF-8')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-})
-
+    optionsSuccessStatus: 204,
+  }));
+  
 app.use(express.json());
 
 app.use("/admin", adminRouter)
